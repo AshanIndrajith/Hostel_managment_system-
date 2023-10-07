fcf665e5283be4876a0ca2885b2f0ffac8993f51
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-10-07 11:58:35 +0530

    update

---
+++ hostalManagmentSystem/hostalManagmentSystem/src/main/java/com/example/hostalManagmentSystem/HostalManagmentSystemApplication.java
@@ -0,0 +1,16 @@
+package com.example.hostalManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostalManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostalManagmentSystemApplication.class, args);
+
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ hostalManagmentSystem/hostalManagmentSystem/src/test/java/com/example/hostalManagmentSystem/HostalManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hostalManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
