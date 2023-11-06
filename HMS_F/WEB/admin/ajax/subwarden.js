5f2b98e25c41bbb47a76f243c7b29a60487d9f0e
Nethmi Rashmika <h.u.g.nethmirashmika@gmail.com>
2023-11-06 15:37:27 +0530

    update

---
+++ HMS_F/WEB/admin/ajax/subwarden.js
@@ -0,0 +1,46 @@
+
+getAllStudent()
+
+function getAllStudent() {
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:8080/api/user/students", // Update with your actual URL
+        success: function (data, status) {
+            console.log("Status:", status);
+            console.log("Data:", data);
+  
+            // Get the table body to populate data
+            var tableBody = $('#subwardenviewStudent tbody');
+            tableBody.empty(); // Clear existing table rows
+  
+            // Loop through the data and create table rows
+            for (var i = 0; i < data.length; i++) {
+                var user = data[i];
+                var id = user[0];
+                var userIndex = user[1];
+                var firstName = user[2];
+                var lastName = user[3];
+                var email = user[5];
+  
+                var newRow = '<tr>' +
+                    '<td>' + id + '</td>' +
+                    '<td>' + userIndex + '</td>' +
+                    '<td>' + firstName + '</td>' +
+                    '<td>' + lastName + '</td>' +
+                    '<td>' + email + '</td>' +
+                    // '<td><button type="button" class="update btn btn-success" onclick="getStudentrDetails(' + id + ')" >Update</button> <button type="button" onclick="deleteStudent(' + id + ') " class="delete btn btn-danger">Delete</button></td>' +
+                   
+                    '</tr>';
+  
+                tableBody.append(newRow);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error("Request failed with status: " + status);
+            console.error("Error details: " + error);
+            console.error("hello");
+        }
+    });
+  }
+  
+  
