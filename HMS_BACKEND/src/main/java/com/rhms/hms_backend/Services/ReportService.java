9c06ec5e57f636089e436c47c0bfc443fbe6675b
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-11-12 09:42:27 +0530

    update

--- HMS_BACKEND/src/main/java/com/rhms/hms_backend/Services/ReportService.java
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Services/ReportService.java
@@ -8,7 +8,6 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
